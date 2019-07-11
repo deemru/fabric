a693dc24e771523bcff458f88a4a38449246e5fe
Dmitrii Pichulin <pdn@cryptopro.ru>
2019-07-11 13:29:29 +0300

    added GOST functionality via supported PLUGIN

--- msp/mspimplvalidate.go
+++ msp/mspimplvalidate.go
@@ -15,10 +15,16 @@ import (
 	"reflect"
 	"time"
 
+	"github.com/hyperledger/fabric/bccsp"
 	"github.com/pkg/errors"
 )
 
 func (msp *bccspmsp) validateIdentity(id *identity) error {
+	if bccsp.IsGostCert(msp.bccsp, id.cert.Raw) {
+		mspLogger.Warnf("GOST cert used (SKIP VALIDATE TODO)")
+		return nil
+	}
+
 	id.validationMutex.Lock()
 	defer id.validationMutex.Unlock()
 
